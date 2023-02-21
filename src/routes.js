d51ef8af27399880ef3fa03cc1e2e82343cb4859
Sitti Rahma <sirah.yh@gmail.com>
2023-02-21 13:34:44 +0800

    first CRUD

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,32 @@
-const {addNoteHandler} = require('./handler')
+const {
+    addNoteHandler, getNoteHandler, getDetailNote, editNoteByIdHandler, deleteNoteByIdHandler
+} = require('./handler')
 
 const routes = [
     {
         method : 'POST',
         path : '/notes',
         handler : addNoteHandler,
+    },
+    {
+        method : 'GET',
+        path : '/notes',
+        handler : getNoteHandler,
+    },
+    {
+        method : 'GET',
+        path : '/notes/{id}',
+        handler : getDetailNote,
+    },
+    {
+        method : 'POST',
+        path : '/notes/{id}',
+        handler : editNoteByIdHandler,
+    }, 
+    {
+        method : 'DELETE',
+        path : '/notes/{id}',
+        handler : deleteNoteByIdHandler,
     }
 ]
 
